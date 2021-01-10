608b9b8880769f8decdfadc94b77f38964873cf0
Zzzasha <77168006+Zzzasha@users.noreply.github.com>
2021-01-10 17:08:44 +0300

    счётчик монет

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CoinController : MonoBehaviour
 {
+    public GameObject TextObject;
+    Text textComponent;
     public AudioClip CoinSound;
-
     public int coin;
+
+    void Start()
+    {
+        textComponent = TextObject.GetComponent<Text>();
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Coin")
         {
-            coin = coin + 1;
+            coin = coin + 1 * 100;
+            textComponent.text = "Score: " + coin.ToString();
             AudioSource.PlayClipAtPoint(CoinSound, transform.position);
             Destroy(other.gameObject);
         }
