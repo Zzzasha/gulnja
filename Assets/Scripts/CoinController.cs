b492f07b70baf8e4982447fc7fe96ac5264941a8
Zzzasha <77168006+Zzzasha@users.noreply.github.com>
2021-01-10 16:29:28 +0300

    Добавил монетки

---
+++ Assets/Scripts/CoinController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour
+{
+    public AudioClip CoinSound;
+
+    public int coin;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Coin")
+        {
+            coin = coin + 1;
+            AudioSource.PlayClipAtPoint(CoinSound, transform.position);
+            Destroy(other.gameObject);
+        }
+    }
+}
