c8f5ae4b4c081365352712d75192162b32846390
Zzzasha <77168006+Zzzasha@users.noreply.github.com>
2021-01-11 00:49:57 +0300

    Экран поражения

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -6,13 +6,16 @@ using UnityEngine.UI;
 public class CoinController : MonoBehaviour
 {
     public GameObject TextObject;
+    public GameObject TextObjectGameOver;
     Text textComponent;
+    Text textComponentGameOver;
     public AudioClip CoinSound;
     public int coin;
 
     void Start()
     {
         textComponent = TextObject.GetComponent<Text>();
+        textComponentGameOver = TextObjectGameOver.GetComponent<Text>();
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -21,6 +24,7 @@ public class CoinController : MonoBehaviour
         {
             coin = coin + 1 * 100;
             textComponent.text = "Score: " + coin.ToString();
+            textComponentGameOver.text = "Score: " + coin.ToString();
             AudioSource.PlayClipAtPoint(CoinSound, transform.position);
             Destroy(other.gameObject);
         }
